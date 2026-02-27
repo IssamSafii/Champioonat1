71ee0d2a1a79cb07a2d548e5b2ac4d3a3aef9b7e
IssamSafii <165966715+IssamSafii@users.noreply.github.com>
2026-02-27 11:05:16 +0100

    WS REST avec Spring

--- src/main/java/tn/esprit/arctic/championnat1/Controller/SponsorController.java
+++ src/main/java/tn/esprit/arctic/championnat1/Controller/SponsorController.java
@@ -1,11 +1,43 @@
 package tn.esprit.arctic.championnat1.Controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import tn.esprit.arctic.championnat1.Services.ISponsorService;
+import tn.esprit.arctic.championnat1.entities.Sponsor;
+
+import java.util.List;
+
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/sponsor")
 public class SponsorController {
     ISponsorService sponsorService ;
+    @PostMapping("/add-sponsor")
+    public Sponsor AjouterSponsor(@RequestBody Sponsor s) {
+        Sponsor sponsor = sponsorService.ajouterSponsor(s);
+        return sponsor;
+    }
+    @GetMapping("/retrieve-all-sponsors")
+    public List<Sponsor> getSponsors() {
+        List<Sponsor> listSponsors = sponsorService.listSponsors();
+        return listSponsors;
+    }
+    @GetMapping("/retrieve-sponsor/{sponsor-id}")
+    public Sponsor retrieveSponsor(@PathVariable("sponsor-id") Long sponsorId) {
+            return sponsorService.recupererSponsor(sponsorId);
+    }
+    @PutMapping("/update-sponsor")
+    public Sponsor updateSponsor(@RequestBody Sponsor s) {
+    Sponsor sponsor = sponsorService.modifierSponsor(s);
+    return sponsor;
+    }
+    @DeleteMapping("/remove-sponsor/{sponsor-id}")
+    public void removeSponsor(@PathVariable("sponsor-id") Long sponsorId) {
+            sponsorService.supprimerSponsor(sponsorId);
+    }
 }
+
+
+
